f5fe013761ab2fa86cc5831eedae75144409c46d
babu <babu@DESKTOP-225PV1Q>
2018-12-10 11:28:25 +0530

    dec 10-1

--- src/github1/example1.java
+++ src/github1/example1.java
@@ -4,7 +4,7 @@ public class example1 {
 
 	public static void main(String[] args) {
 		System.out.println("hello example1");
-		System.out.println("suchi");
+		System.out.println("fnsknfknfk");
 	}
 
 }
